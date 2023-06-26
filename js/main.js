f835adcfb1e3650c396f6568516d241c11bd78e2
Valerio Billi <valerio.billi18@gmail.com>
2023-06-26 16:21:12 +0200

    get 5 random numbers

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+const randomNumbers = [];
+while (randomNumbers.length < 5) {
+
+    const number = getRandomNumber(1, 100);
+
+    if (randomNumbers.includes(number) == false) {
+        randomNumbers.push(number);
+    }
+}
+
+document.getElementById("numbers").innerText = randomNumbers;
+
+
+console.log(randomNumbers);
+
+
+
+// funzioni
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
